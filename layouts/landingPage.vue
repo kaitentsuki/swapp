2b481c0ece9b06217b0b74399a73ce77034ea156
Lukáš Kalný <34368048+kaitentsuki@users.noreply.github.com>
2023-11-13 15:29:56 +0100

    landing page layout centered

--- layouts/landingPage.vue
+++ layouts/landingPage.vue
@@ -3,10 +3,16 @@
         <div class="px-20 w-full flex flex-col">
             <LayoutNavbar />
             <div class="">
-                <div class="pt-40 bg-gray-900 -mx-20 -mt-3 px-20 pb-10">
-                    <h1 class="font-bold text-lime-400 text-5xl leading-[1.2] uppercase">Co děláme</h1>
+                <div class="pt-40 bg-gray-900 -mx-20 -mt-3 px-20 pb-10 flex justify-center">
+                    <div class="max-w-7xl w-full">
+                        <h1 class="font-bold text-lime-400 text-5xl leading-[1.2] uppercase">Co děláme</h1>
+                    </div>
                 </div>
-            <NuxtPage />
+            <div class="flex justify-center">
+                <div class="max-w-7xl w-full">
+                    <NuxtPage />
+                </div>
+            </div>
             </div>
             <LayoutFooter />
         </div>
